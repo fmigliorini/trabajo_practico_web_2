a335ff19469b18f0615f1380bc47866147fde82b
Facundo Migliorini <facundo.mig@gmail.com>
2017-11-09 13:09:40 -0300

    reordeno el menu

--- templates/menu.php
+++ templates/menu.php
@@ -7,12 +7,13 @@
     <!-- Sidebar Menu -->
     <ul class="sidebar-menu" data-widget="tree">
       <!-- Optionally, you can add icons to the links -->
-      <li><a href="#"><i class="fa fa-link"></i> <span>Viajes</span></a></li>
+      <li <?php echo ( Helper::isPage('roles') ) ? 'class="active"' : '' ?> ><a href="index.php?page=roles"><i class="fa fa-link"></i> <span>Roles</span></a></li>
+      <li <?php echo ( Helper::isPage('usuario') ) ? 'class="active"' : '' ?>><a href="index.php?page=usuario"><i class="fa fa-link"></i> <span>Usuarios</span></a></li>
+
       <li><a href="index.php?page=empleado"><i class="fa fa-link"></i> <span>Empleados</span></a></li>
       <li><a href="#"><i class="fa fa-link"></i> <span>Clientes</span></a></li>
+      <li><a href="#"><i class="fa fa-link"></i> <span>Viajes</span></a></li>
       <li><a href="#"><i class="fa fa-link"></i> <span>Vehiculos</span></a></li>
-      <li <?php echo ( Helper::isPage('usuario') ) ? 'class="active"' : '' ?>><a href="index.php?page=usuario"><i class="fa fa-link"></i> <span>Usuarios</span></a></li>
-      <li <?php echo ( Helper::isPage('roles') ) ? 'class="active"' : '' ?> ><a href="index.php?page=roles"><i class="fa fa-link"></i> <span>Roles</span></a></li>
       <li><a href="#"><i class="fa fa-link"></i> <span>Mantenimiento de Vehiculos</span></a></li>
     </ul>
     <!-- /.sidebar-menu -->
