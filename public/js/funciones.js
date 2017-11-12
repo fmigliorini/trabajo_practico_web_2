82b21e66cb008fb692f42469fd74db91648567fc
Federico Rastelli <federaste14@gmail.com>
2017-11-12 18:13:56 -0300

    Cambios de directorio modificado

--- public/js/funciones.js
+++ public/js/funciones.js
@@ -1,5 +1,5 @@
 $(function(){
-	$("#tablaRoles").dataTable({
+	$("table").dataTable({
 		"ordering": false,
 		"info": false,
 		"lengthChange": false,
@@ -16,26 +16,6 @@ $(function(){
 		        "next":       "Siguiente",
 		        "previous":   "Anterior",
 		    },
-		},
-
-	});
-	$("#tablaUsuarios").dataTable({
-		"ordering": false,
-		"info": false,
-		"lengthChange": false,
-		"searching": false,
-		"language": {
-			"emptyTable":     "No hay resultados",
-			"loadingRecords": "Loading...",
-			"processing":     "Procesando...",
-			"search":         "Buscar:",
-			"zeroRecords":    "No hay resultados",
-			"paginate": {
-				"first":      "Primero",
-				"last":       "Ultimo",
-				"next":       "Siguiente",
-				"previous":   "Anterior",
-			},
 		},
 
 	});
